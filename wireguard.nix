8f8978ff104cbb2cd190e9f5b5ead94d163a3b74
David Wronek <david@mainlining.org>
2025-05-27 09:28:10 +0200

    wireguard: Shorten IPv6 addresses

--- wireguard.nix
+++ wireguard.nix
@@ -33,13 +33,13 @@ in
         };
         wireguardPeers = [
           {
-            AllowedIPs = [ "192.168.128.2/32" "fd49:1279:1d2e:5063::2/128" ];
+            AllowedIPs = [ "192.168.128.2/32" "fd96::2/128" ];
             PersistentKeepalive = 25;
             PresharedKeyFile = config.sops.secrets."wireguard/rpi5/psk-1".path;
             PublicKey = "YRC+IrnNovLJWW3qXi0LydpwuMQ8NtaH+9I6/rVzXHQ=";
           }
           {
-            AllowedIPs = [ "192.168.128.3/32" "fd49:1279:1d2e:5063::3/128"];
+            AllowedIPs = [ "192.168.128.3/32" "fd96::3/128"];
             PersistentKeepalive = 25;
             PresharedKeyFile = config.sops.secrets."wireguard/rpi5/psk-2".path;
             PublicKey = "3aqGBPeBmtL9n4yqX7kG5TZ8yugsr0iQWq2WZlBxbzk=";
@@ -49,7 +49,7 @@ in
     };
     networks.wg0 = {
       matchConfig.Name = "wg0";
-      address = [ "192.168.128.1/24" "fd49:1279:1d2e:5063::1/64" ];
+      address = [ "192.168.128.1/24" "fd96::1/64" ];
       networkConfig = {
         IPMasquerade = "both";
       };
