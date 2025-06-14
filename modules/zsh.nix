b88a9291986400c88917c79c894d74b8201933ed
David Wronek <david@mainlining.org>
2025-06-14 21:45:55 +0200

    zsh: Make Heimdall wait for device

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -3,6 +3,7 @@
 let
   ffmpeg = lib.getExe pkgs.ffmpeg;
   git = lib.getExe pkgs.git;
+  heimdall = lib.getExe pkgs.heimdall;
   readelf = lib.getExe' pkgs.binutils "readelf";
   yt-dlp = lib.getExe pkgs.yt-dlp;
 in
@@ -27,6 +28,7 @@ in
     shellAliases = {
       compress-vid = "${ffmpeg} -vcodec libx264 -crf 28 output.mp4 -i";
       rp = "realpath";
+      heimdall = "heimdall-wait-for-device && ${heimdall}";
       switch-nixos = "sudo nixos-rebuild switch --flake path:.#${config.networking.hostName}";
       yt-dlp-mp4 = "${yt-dlp} -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'";
     };
@@ -63,6 +65,12 @@ in
 
       export WORDCHARS="''${WORDCHARS//\/}"
 
+      heimdall-wait-for-device () {
+	echo "< waiting for any device >"
+	while ! ${heimdall} detect > /dev/null 2>&1; do
+	    sleep 1
+	done
+      }
       libneeds () {${readelf} -d $1 |grep '\(NEEDED\)' | sed -r 's/.*\[(.*)\]/\1/'}
       precmd () {
           gitinfo=$(${git} branch --show-current 2> /dev/null)
