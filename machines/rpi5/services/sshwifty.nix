e23002e296cf033d859b64a2fc12e109a24f24bd
David Wronek <david@mainlining.org>
2025-06-24 07:14:27 +0200

    sshwifty: Generate preset automatically

--- machines/rpi5/services/sshwifty.nix
+++ machines/rpi5/services/sshwifty.nix
@@ -1,4 +1,4 @@
-{ config, lib, inputs, vars, ... }:
+{ config, lib, pkgs, inputs, vars, ... }:
 
 let
   domain = "ssh.${baseDomain}";
@@ -8,6 +8,32 @@ let
 
   arch = config.nixpkgs.hostPlatform.system;
   sshwifty = lib.getExe inputs.self.packages.${arch}.sshwifty;
+  hostKey = "/etc/ssh/ssh_host_rsa_key.pub";
+  genPreset = pkgs.writers.writePython3 "sshwifty-genpreset" { } ''
+    from base64 import b64decode, b64encode
+    from hashlib import sha256
+    from json import dumps
+
+    with open('/etc/ssh/ssh_host_rsa_key.pub', 'r') as keyfile:
+        pubkey = keyfile.read().split(' ')[1]
+        sha256 = sha256()
+        sha256.update(b64decode(pubkey))
+        b64fingerprint = b64encode(sha256.digest()).decode().split('=')[0]
+        preset = [
+            {
+                'Title': 'rpi5',
+                'Type': 'SSH',
+                'Host': 'rpi5:22',
+                'Meta': {
+                    'User': 'root',
+                    'Encoding': 'utf-8',
+                    'Authentication': 'Private Key',
+                    'Fingerprint': f'SHA256:{b64fingerprint}'
+                }
+            }
+        ]
+        print(f"SSHWIFTY_PRESETS='{dumps(preset)}'")
+  '';
 in
 {
   networking.hosts."::1" = [ domain ];
@@ -35,8 +61,12 @@ in
       SSHWIFTY_LISTENINTERFACE = "127.0.0.1";
       SSHWIFTY_LISTENPORT = "8089";
     };
+    script = ''
+      eval $(${genPreset})
+      export SSHWIFTY_PRESETS
+      exec ${sshwifty}
+    '';
     serviceConfig = {
-      ExecStart = sshwifty;
       EnvironmentFile = config.sops.secrets."sshwifty/sharedkey".path;
       DynamicUser = true;
       ProtectSystem = "strict";
@@ -44,7 +74,7 @@ in
       PrivateTmp = true;
       PrivateDevices = true;
       RemoveIPC = true;
-      RestrictsUIDSGID = true;
+      RestrictSUIDSGID = true;
     };
   };
 }
