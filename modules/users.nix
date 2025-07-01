c9d392241d241f66ea18c06dba6c509cda20dee9
David Wronek <david@mainlining.org>
2025-07-01 20:41:14 +0200

    users: add home-assistant SSH key for ryuzu

--- modules/users.nix
+++ modules/users.nix
@@ -17,7 +17,10 @@
         ++ lib.optionals config.programs.wireshark.enable [ "wireshark" ]
         ++ lib.optionals config.virtualisation.libvirtd.enable [ "libvirt" ];
       hashedPasswordFile = config.sops.secrets."users/david".path;
-      openssh.authorizedKeys.keys = vars.sshPubKeys;
+      openssh.authorizedKeys.keys = vars.sshPubKeys
+        ++ lib.optionals (config.networking.hostName == "ryuzu") [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHCo1aBmLiSmZrjtnFVYczuv4/cNXjxF+4soUTSIeZla hass"
+        ];
     } // lib.optionalAttrs config.programs.zsh.enable { shell = pkgs.zsh; };
     users.root = {
       openssh.authorizedKeys.keys = vars.sshPubKeys;
