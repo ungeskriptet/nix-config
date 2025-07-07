baca63dc35796b147d8365c78b3d7c1d9a2dfc5b
David Wronek <david@mainlining.org>
2025-07-07 08:22:46 +0200

    users: add option for username and description

--- modules/users.nix
+++ modules/users.nix
@@ -1,29 +1,47 @@
 { config, inputs, lib, pkgs, vars, ... }:
 
+let
+  cfg = config.users;
+in
 {
-  sops = {
-    secrets."users/david".neededForUsers = true;
-    secrets."users/david".owner = "root";
+  options.users = {
+    userName = lib.mkOption {
+      type = lib.types.str;
+      default = "david";
+    };
+    userDescription = lib.mkOption {
+      type = lib.types.str;
+      default = "David";
+    };
   };
 
-  users = {
-    mutableUsers = false;
-    users.david = {
-      isNormalUser = true;
-      description = "David";
-      extraGroups =
-        [ "wheel" ]
-        ++ lib.optionals config.networking.networkmanager.enable [ "networkmanager" ]
-        ++ lib.optionals config.programs.wireshark.enable [ "wireshark" ]
-        ++ lib.optionals config.virtualisation.libvirtd.enable [ "libvirt" ];
-      hashedPasswordFile = config.sops.secrets."users/david".path;
-      openssh.authorizedKeys.keys = vars.sshPubKeys
-        ++ lib.optionals (config.networking.hostName == "ryuzu") [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHCo1aBmLiSmZrjtnFVYczuv4/cNXjxF+4soUTSIeZla hass"
-        ];
-    } // lib.optionalAttrs config.programs.zsh.enable { shell = pkgs.zsh; };
-    users.root = {
-      openssh.authorizedKeys.keys = vars.sshPubKeys;
-    } // lib.optionalAttrs config.programs.zsh.enable { shell = pkgs.zsh; };
+  config = {
+    sops = {
+      secrets."users/${cfg.userName}" = {
+        neededForUsers = true;
+        owner = "root";
+      };
+    };
+
+    users = {
+      mutableUsers = false;
+      users.${cfg.userName} = {
+        isNormalUser = true;
+        description = cfg.userDescription;
+        extraGroups =
+          [ "wheel" ]
+          ++ lib.optionals config.networking.networkmanager.enable [ "networkmanager" ]
+          ++ lib.optionals config.programs.wireshark.enable [ "wireshark" ]
+          ++ lib.optionals config.virtualisation.libvirtd.enable [ "libvirt" ];
+        hashedPasswordFile = config.sops.secrets."users/${cfg.userName}".path;
+        openssh.authorizedKeys.keys = vars.sshPubKeys
+          ++ lib.optionals (config.networking.hostName == "ryuzu") [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHCo1aBmLiSmZrjtnFVYczuv4/cNXjxF+4soUTSIeZla hass"
+          ];
+      } // lib.optionalAttrs config.programs.zsh.enable { shell = pkgs.zsh; };
+      users.root = {
+        openssh.authorizedKeys.keys = vars.sshPubKeys;
+      } // lib.optionalAttrs config.programs.zsh.enable { shell = pkgs.zsh; };
+    };
   };
 }
