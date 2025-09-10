bf8e166a41849f9452ded23067201a4d5e1769e4
David Wronek <david@mainlining.org>
2025-09-10 10:38:05 +0200

    home: david: add GrooveStats.ini

--- home/david/desktop.nix
+++ home/david/desktop.nix
@@ -1,7 +1,26 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  ...
+}:
 {
   imports = [
     ./common.nix
     ./firefox
   ];
+
+  sops = {
+    secrets."groovestats/apikey" = { };
+    templates."GrooveStats.ini".content = ''
+      [GrooveStats]
+      ApiKey=${config.sops.placeholder."groovestats/apikey"}
+      IsPadPlayer=1
+    '';
+  };
+
+  home.file = {
+    ".itgmania/Save/LocalProfiles/00000000/GrooveStats.ini".source =
+      config.lib.file.mkOutOfStoreSymlink
+        config.sops.templates."GrooveStats.ini".path;
+  };
 }
