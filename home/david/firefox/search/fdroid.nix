b14440b957937943c26dcad15d2c7a120af150b7
David Wronek <david.wronek@mainlining.org>
2025-09-13 12:30:35 +0200

    home: david: firefox: add F-Droid search

---
+++ home/david/firefox/search/fdroid.nix
@@ -0,0 +1,9 @@
+{ ... }:
+{
+  fdroid = {
+    name = "F-Droid";
+    urls = [ { template = "https://search.f-droid.org/?q={searchTerms}"; } ];
+    icon = "https://f-droid.org/assets/fdroid-logo_bfHl7nsLHOUQxzdU8-rGIhn4bAgl6z7k2mA3fWoCyT4=.png";
+    definedAliases = [ "@fd" ];
+  };
+}
