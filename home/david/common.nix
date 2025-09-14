a351285d3197c52f65b904d360b536777fcd3f1e
David Wronek <david.wronek@mainlining.org>
2025-09-14 10:12:38 +0200

    home: david: set up patatt

--- home/david/common.nix
+++ home/david/common.nix
@@ -23,6 +23,10 @@ in
         path = "${homeDir}/.android/adbkey";
         mode = "0400";
       };
+      "patatt/privkey" = {
+        path = "${homeDir}/.local/share/patatt/private/20250914.key";
+        mode = "0400";
+      };
       "ssh/privkey" = {
         path = "${homeDir}/.ssh/id_ed25519";
         mode = "0400";
@@ -36,6 +40,12 @@ in
       lfs.enable = true;
       userName = config.myuser.realName;
       userEmail = "david.wronek@mainlining.org";
+      extraConfig = {
+        patatt = {
+          signingkey = "ed25519:20250914";
+          selector = "20250914";
+        };
+      };
     };
     home-manager.enable = true;
   };
@@ -50,6 +60,13 @@ in
       ".android/adbkey.pub".source = ./dotfiles/adbkey.pub;
       ".ssh/config".text = import ./dotfiles/ssh/config.nix { inherit lib pkgs; };
       ".ssh/id_ed25519.pub".source = ./dotfiles/ssh/id_ed25519.pub;
-    };
+    }
+    // lib.mergeAttrsList (
+      builtins.map (file: { ".local/share/patatt/public/${file}".source = ./dotfiles/patattkey.pub; }) [
+        "ed25519/mainlining.org/david.wronek/20250914"
+        "ed25519/mainlining.org/david.wronek/default"
+        "20250914.pub"
+      ]
+    );
   };
 }
