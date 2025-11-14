e2d8d36d557c4ca6e113494a7cd409d8b9db9f53
David Wronek <david.wronek@mainlining.org>
2025-11-14 08:20:26 +0100

    rpi5: import services automatically

--- machines/rpi5/services/default.nix
+++ machines/rpi5/services/default.nix
@@ -1,28 +1,10 @@
-{ ... }:
+{ lib, ... }:
 {
-  imports = [
-    ./acme.nix
-    ./adguardhome.nix
-    ./caddy.nix
-    ./esphome.nix
-    ./firefox.nix
-    ./home-assistant.nix
-    ./immich.nix
-    ./mollysocket.nix
-    ./nextcloud.nix
-    ./ntfy-sh.nix
-    ./openvscode-server.nix
-    ./paperless-ngx.nix
-    ./phonetrack-notify.nix
-    ./postgresql.nix
-    ./roundcube.nix
-    ./samsung-grab.nix
-    ./samsung-update-bot.nix
-    ./soju.nix
-    ./sshwifty.nix
-    ./stalwart-mail.nix
-    ./vaultwarden.nix
-    ./wireguard.nix
-    ./yuribot.nix
-  ];
+  imports = lib.map (file: ./. + "/${file}") (
+    lib.attrNames (
+      lib.filterAttrs (
+        file: type: file != "default.nix" && type == "regular" && lib.hasSuffix ".nix" file
+      ) (builtins.readDir ./.)
+    )
+  );
 }
