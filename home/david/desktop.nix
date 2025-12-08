d029b2fea453c8357a81c66d763464b11497a0fc
David Wronek <david.wronek@mainlining.org>
2025-12-08 10:19:23 +0100

    home: david: desktop: add opencloud-desktop

--- home/david/desktop.nix
+++ home/david/desktop.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   ...
 }:
 {
@@ -19,10 +20,23 @@
     '';
   };
 
-  home.file = {
-    ".itgmania/Save/LocalProfiles/00000000/GrooveStats.ini" = {
-      source = config.lib.file.mkOutOfStoreSymlink config.sops.templates."GrooveStats.ini".path;
-      force = true;
+  xdg = {
+    enable = true;
+    autostart = {
+      enable = true;
+      entries = with pkgs; [
+        "${opencloud-desktop}/share/applications/opencloud.desktop"
+      ];
     };
+  };
+
+  home = {
+    file = {
+      ".itgmania/Save/LocalProfiles/00000000/GrooveStats.ini" = {
+        source = config.lib.file.mkOutOfStoreSymlink config.sops.templates."GrooveStats.ini".path;
+        force = true;
+      };
+    };
+    packages = with pkgs; [ opencloud-desktop ];
   };
 }
