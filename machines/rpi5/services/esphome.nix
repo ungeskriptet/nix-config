1e0f7931e7476c776be08e7476cd57075628b941
David Wronek <david.wronek@mainlining.org>
2025-12-09 20:30:10 +0100

    rpi5: esphome: change secrets owner to root
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- machines/rpi5/services/esphome.nix
+++ machines/rpi5/services/esphome.nix
@@ -8,35 +8,19 @@ let
   domain = config.networking.domain;
 in
 {
-  users = {
-    groups.esphome = { };
-    users.esphome = {
-      isSystemUser = true;
-      group = "esphome";
-      home = "/var/lib/esphome";
-      homeMode = "750";
-      createHome = true;
-    };
-  };
-
-  sops.secrets."esphome/env".owner = "esphome";
+  sops.secrets."esphome/env".owner = "root";
 
   networking.hosts."::1" = [ fqdn ];
   networking.hosts."127.0.0.1" = [ fqdn ];
 
-  systemd.services.caddy = {
-    serviceConfig.SupplementaryGroups = "esphome";
-  };
+  systemd.services = {
+    esphome = {
+      serviceConfig.EnvironmentFile = config.sops.secrets."esphome/env".path;
+    };
 
-  systemd.services.esphome = {
-    serviceConfig = {
-      EnvironmentFile = config.sops.secrets."esphome/env".path;
-      User = "esphome";
-      Group = "esphome";
-      DynamicUser = lib.mkForce false;
-      PrivateTmp = lib.mkForce true;
-      RemoveIPC = lib.mkForce true;
-      RestrictSUIDSGID = lib.mkForce true;
+    caddy = {
+      wants = [ config.systemd.services.esphome.name ];
+      serviceConfig.SupplementaryGroups = [ "esphome" ];
     };
   };
 
