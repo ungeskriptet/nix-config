bb83054545cb1aecc0b8c1cbb1a0073e6ac8bf95
David Wronek <david.wronek@mainlining.org>
2026-01-20 07:41:21 +0100

    home: david: thunderbird: install dictionary-german
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- home/david/accounts/desktop.nix
+++ home/david/accounts/desktop.nix
@@ -12,8 +12,10 @@
     enable = true;
     profiles."Default" = {
       isDefault = true;
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [ dictionary-german ];
       settings = {
         "datareporting.healthreport.uploadEnabled" = false;
+        "extensions.autoDisableScopes" = 0;
         "intl.date_time.pattern_override.date_short" = "dd.MM.yyyy";
         "intl.date_time.pattern_override.time_short" = "HH:mm";
         "mail.compose.default_to_paragraph" = false;
