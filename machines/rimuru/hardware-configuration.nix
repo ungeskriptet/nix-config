4302ba13c7e3b8a319f0f7da6a60ad2226c73dc0
David Wronek <david.wronek@mainlining.org>
2026-03-03 19:34:31 +0100

    rimuru: update disk UUIDs
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- machines/rimuru/hardware-configuration.nix
+++ machines/rimuru/hardware-configuration.nix
@@ -27,12 +27,12 @@
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/e5672b62-e1cf-459a-9503-b7f2c063d406";
+      device = "/dev/disk/by-uuid/4b23f0f4-5e0c-45cc-8bb7-c0554f6413ae";
       fsType = "ext4";
     };
 
     "/boot" = {
-      device = "/dev/disk/by-uuid/8BD2-80B7";
+      device = "/dev/disk/by-uuid/3103-22DD";
       fsType = "vfat";
       options = [
         "fmask=0077"
