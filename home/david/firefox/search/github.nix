67ba0443e60c6acaebb252a2e164bf9f797ad3bf
David Wronek <david.wronek@mainlining.org>
2026-03-19 09:47:03 +0100

    home: david: firefox: add LineageOS GitHub search
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- home/david/firefox/search/github.nix
+++ home/david/firefox/search/github.nix
@@ -20,4 +20,24 @@
     icon = "https://github.githubassets.com/assets/pinned-octocat-093da3e6fa40.svg";
     definedAliases = [ ":gh" ];
   };
+  lineageos-github = {
+    name = "LineageOS (GitHub)";
+    urls = [
+      {
+        template = "https://github.com/search";
+        params = [
+          {
+            name = "q";
+            value = "org%3ALineageOS+{searchTerms}";
+          }
+          {
+            name = "type";
+            value = "code";
+          }
+        ];
+      }
+    ];
+    icon = "https://lineageos.org/images/logo.webp";
+    definedAliases = [ ":los" ];
+  };
 }
