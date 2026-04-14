13c230f1a2415f06f6492067dc55e9ec7a91d6ef
David Wronek <david.wronek@mainlining.org>
2026-04-14 13:02:30 +0200

    home: david: firefox: disable translate popup
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- home/david/firefox/settings.nix
+++ home/david/firefox/settings.nix
@@ -3,6 +3,7 @@
   "browser.ctrlTab.sortByRecentlyUsed" = true;
   "browser.newtabpage.activity-stream.showSponsoredCheckboxes" = false;
   "browser.startup.page" = 3;
+  "browser.translations.automaticallyPopup" = false;
   "extensions.autoDisableScopes" = 0;
   "general.autoScroll" = true;
   "widget.use-xdg-desktop-portal.file-picker" = 1;
