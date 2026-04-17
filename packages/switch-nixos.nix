e120aa91d17c3bbaf6016eeb5d067db4123e28c6
David Wronek <david.wronek@mainlining.org>
2026-04-17 08:34:24 +0200

    switch-nixos: accept flake config and pass $*
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- packages/switch-nixos.nix
+++ packages/switch-nixos.nix
@@ -6,19 +6,16 @@ writeShellScriptBin "switch-nixos" ''
 
   Options:
   -f | --flake    Flake URL (Default: path:/etc/nixos#$(hostname))
-  -t | --target   Deploy to remote host through SSH
   -h | --help     Show help and exit
   EOF
   }
 
   # Defaults
-  target=""
   flake="path:/etc/nixos#$(hostname)"
 
   while [ $# -gt 0 ]; do
     case "$1" in
       --flake|-f) flake="$2"; shift 2 ;;
-      --target|-t) target="--target-host $2"; shift 2 ;;
       --help|-h) help; exit ;;
       *) break ;;
     esac
@@ -37,5 +34,5 @@ writeShellScriptBin "switch-nixos" ''
 
   sudo \
     NIX_SSHOPTS="''${NIXOS_SSHOPTS[*]}" nixos-rebuild \
-    switch -L --flake $flake $target
+    switch -L --accept-flake-config --flake $flake $*
 ''
