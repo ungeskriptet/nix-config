ead029ab7c257f326abde7b843b228490d8f7ee4
David Wronek <david.wronek@mainlining.org>
2026-05-05 10:44:02 +0200

    home: david: firefox: block AI
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- home/david/firefox/policies.nix
+++ home/david/firefox/policies.nix
@@ -1,14 +1,17 @@
 {
+  # keep-sorted start block=yes
+  AIControls = {
+    Default = {
+      Value = "blocked";
+      Locked = true;
+    };
+  };
   AutofillAddressEnabled = false;
   AutofillCreditCardEnabled = false;
   DisableAppUpdate = true;
   DisableFirefoxStudies = true;
   DisablePocket = true;
   DisableTelemetry = true;
-  OfferToSaveLogins = false;
-  OverrideFirstRunPage = "";
-  OverridePostUpdatePage = "";
-  PasswordManagerEnabled = false;
   FirefoxHome = {
     TopSites = false;
     SponsoredTopSites = false;
@@ -23,4 +26,10 @@
     SponsoredSuggestions = false;
     ImproveSuggest = false;
   };
+  NoDefaultBookmarks = true;
+  OfferToSaveLogins = false;
+  OverrideFirstRunPage = "";
+  OverridePostUpdatePage = "";
+  PasswordManagerEnabled = false;
+  # keep-sorted end
 }
