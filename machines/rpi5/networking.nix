67af9d3111a3b7832dd6370a6df09c980d2306af
David Wronek <david.wronek@mainlining.org>
2026-07-24 21:47:49 +0200

    rpi5: networking: add secondary loopback address
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- machines/rpi5/networking.nix
+++ machines/rpi5/networking.nix
@@ -31,37 +31,43 @@
 
     systemd.network = {
       enable = true;
-      networks."10-lan" = {
-        matchConfig.Name = "end0";
-        linkConfig.RequiredForOnline = "routable";
-        DHCP = "no";
-        address = [
-          "${config.networking.lanIPv4}/24"
-          "${config.networking.lanIPv6}/64"
-          "${config.networking.globalIpv6}/64"
-        ]
-        ++ lib.optionals config.services.adguardhome.enable [
-          config.networking.adGuardIpv4
-          config.networking.adGuardIpv6
-        ];
-        gateway = [ config.networking.gatewayIP ];
-        domains = [ config.networking.lanDomain ];
-        dns =
-          lib.optionals config.services.adguardhome.enable [
-            "::1"
-            "127.0.0.1"
+      networks = {
+        "10-lan" = {
+          matchConfig.Name = "end0";
+          linkConfig.RequiredForOnline = "routable";
+          DHCP = "no";
+          address = [
+            "${config.networking.lanIPv4}/24"
+            "${config.networking.lanIPv6}/64"
+            "${config.networking.globalIpv6}/64"
           ]
-          ++ [
-            "2001:678:e68:f000::"
-            "5.1.66.255"
+          ++ lib.optionals config.services.adguardhome.enable [
+            "${config.networking.adGuardIpv4}/24"
+            "${config.networking.adGuardIpv6}/64"
           ];
-        networkConfig = {
-          IPv6AcceptRA = true;
-          IPv6PrivacyExtensions = false;
+          gateway = [ config.networking.gatewayIP ];
+          domains = [ config.networking.lanDomain ];
+          dns =
+            lib.optionals config.services.adguardhome.enable [
+              "::1"
+              "127.0.0.1"
+            ]
+            ++ [
+              "2001:678:e68:f000::"
+              "5.1.66.255"
+            ];
+          networkConfig = {
+            IPv6AcceptRA = true;
+            IPv6PrivacyExtensions = false;
+          };
+          ipv6AcceptRAConfig = {
+            Token = "prefixstable";
+            UseDNS = false;
+          };
         };
-        ipv6AcceptRAConfig = {
-          Token = "prefixstable";
-          UseDNS = false;
+        "20-lo" = {
+          matchConfig.Name = "lo";
+          address = [ "::2/128" ];
         };
       };
     };
