6412ecd53e39e7c3c481f7d4edee33b3e7ae6df2
David Wronek <david.wronek@mainlining.org>
2026-08-30 09:54:46 +0200

    rpi5: prometheus: add new service
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

---
+++ machines/rpi5/services/prometheus.nix
@@ -0,0 +1,56 @@
+{
+  config,
+  ...
+}:
+let
+  fqdn = "prometheus.${domain}";
+  domain = config.networking.domain;
+in
+{
+  sops.secrets = {
+    "stalwart/prometheus-pass".owner = "root";
+  };
+
+  networking = {
+    hosts = {
+      "::1" = [
+        fqdn
+      ];
+      "127.0.0.1" = [
+        fqdn
+      ];
+    };
+  };
+
+  systemd.services = {
+    prometheus.serviceConfig = {
+      LoadCredential = [
+        "stalwart:${config.sops.secrets."stalwart/prometheus-pass".path}"
+      ];
+    };
+  };
+
+  services = {
+    prometheus = {
+      enable = true;
+      listenAddress = "[::1]";
+      port = 8077;
+      scrapeConfigs = [
+        {
+          job_name = "stalwart";
+          metrics_path = "/metrics/prometheus";
+          scheme = "https";
+          static_configs = [
+            {
+              targets = [ "mail.${domain}:443" ];
+            }
+          ];
+          basic_auth = {
+            username = "prometheus";
+            password_file = "/run/credentials/prometheus.service/stalwart";
+          };
+        }
+      ];
+    };
+  };
+}
