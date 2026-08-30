fb40b6ffac8fb44f9f1f10eea2ee308f560f132e
David Wronek <david.wronek@mainlining.org>
2026-08-30 14:58:38 +0200

    zsh: aliases: don't hardcode nix store paths
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- modules/zsh/common.nix
+++ modules/zsh/common.nix
@@ -24,71 +24,58 @@ in
         "HIST_IGNORE_SPACE"
       ];
       enableCompletion = true;
-      shellAliases =
-        let
-          bat = lib.getExe pkgs.bat;
-          ffmpeg = lib.getExe pkgs.ffmpeg;
-          heimdall = lib.getExe pkgs.heimdall;
-          yt-dlp = lib.getExe pkgs.yt-dlp;
-        in
-        {
-          # keep-sorted start
-          f = "nix fmt";
-          g = "git";
-          gc = "nh clean all";
-          gco = "nh clean all --optimise";
-          gr = "cd $(git rev-parse --show-toplevel)";
-          j = "journalctl";
-          jeu = "journalctl -eu";
-          jfu = "journalctl -fu";
-          ls = "ls --color=auto";
-          n = "nix";
-          nb = "nix-build";
-          nd = "nix develop";
-          nr = "nix run";
-          ns = "nix-shell";
-          rp = "realpath";
-          s = "systemctl";
-          sc = "systemctl cat";
-          start = "systemctl start";
-          status = "systemctl status";
-          stop = "systemctl stop";
-          switch-nixos = "nh os switch /etc/nixos -L --accept-flake-config";
-          v = "vim";
-          wineprefix = "export WINEPREFIX=$(mktemp -d --suffix -wine)";
-          # keep-sorted end
-        }
-        // lib.optionalAttrs cfg.david.enable {
-          c = "${bat} -pp";
-          rpi5 = "ssh root@rpi5";
-          ryuzu = "ssh david@ryuzu";
-          tftp-server = "sudo mkdir -p -m a=rwx tftp; sudo in.tftpd --foreground --listen --address :69 --secure --create ./tftp";
-        }
-        // lib.optionalAttrs (!cfg.nixOnDroid.enable && cfg.david.enable) {
-          compress-vid = "${ffmpeg} -vcodec libx264 -crf 28 output.mp4 -i";
-          heimdall = "heimdall-wait-for-device && ${heimdall}";
-          yt-dlp-mp4 = "${yt-dlp} -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'";
-        }
-        // lib.optionalAttrs (!cfg.homeManager.enable) {
-          nixpkgs-info = lib.concatStringsSep " " [
-            "nix flake metadata nix-config --json |"
-            "jq '.locks.nodes.root.inputs.nixpkgs as $nixpkgs |"
-            ".locks.nodes | to_entries[] |"
-            "select(.key == $nixpkgs)'"
-          ];
-        }
-        // lib.optionalAttrs cfg.nixOnDroid.enable {
-          ping = "/system/bin/ping";
-          start-sshd = "${lib.getExe' cfg.nixOnDroid.opensshPkg "sshd"} -f ${pkgs.writeText "sshd_config" ''
-            HostKey /etc/ssh/ssh_host_ed25519_key
-            Port 8022
-            AllowUsers nix-on-droid
-            PasswordAuthentication No
-            KbdInteractiveAuthentication No
-            Subsystem sftp ${cfg.nixOnDroid.opensshPkg}/libexec/sftp-server
-          ''} -D";
-          switch-nixondroid = "nix-on-droid switch -F path:/data/data/com.termux.nix/files/home/.config/nix-on-droid#nix-on-droid";
-        };
+      shellAliases = {
+        # keep-sorted start block=yes
+        c = "bat -pp";
+        compress-vid = "ffmpeg -vcodec libx264 -crf 28 output.mp4 -i";
+        f = "nix fmt";
+        g = "git";
+        gc = "nh clean all";
+        gco = "nh clean all --optimise";
+        gr = "cd $(git rev-parse --show-toplevel)";
+        heimdall = "heimdall-wait-for-device";
+        j = "journalctl";
+        jeu = "journalctl -eu";
+        jfu = "journalctl -fu";
+        ls = "ls --color=auto";
+        n = "nix";
+        nb = "nix-build";
+        nd = "nix develop";
+        nixpkgs-info = lib.concatStringsSep " " [
+          "nix flake metadata nix-config --json |"
+          "jq '.locks.nodes.root.inputs.nixpkgs as $nixpkgs |"
+          ".locks.nodes | to_entries[] |"
+          "select(.key == $nixpkgs)'"
+        ];
+        nr = "nix run";
+        ns = "nix-shell";
+        rp = "realpath";
+        rpi5 = "ssh root@rpi5";
+        ryuzu = "ssh david@ryuzu";
+        s = "systemctl";
+        sc = "systemctl cat";
+        start = "systemctl start";
+        status = "systemctl status";
+        stop = "systemctl stop";
+        switch-nixos = "nh os switch /etc/nixos -L --accept-flake-config";
+        tftp-server = "sudo mkdir -p -m a=rwx tftp; sudo in.tftpd --foreground --listen --address :69 --secure --create ./tftp";
+        v = "vim";
+        wineprefix = "export WINEPREFIX=$(mktemp -d --suffix -wine)";
+        yt-dlp-mp4 = "yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'";
+        # keep-sorted end
+      }
+      // lib.optionalAttrs cfg.nixOnDroid.enable {
+        ping = "/system/bin/ping";
+        start-sshd = "${lib.getExe' cfg.nixOnDroid.opensshPkg "sshd"} -f ${pkgs.writeText "sshd_config" ''
+          HostKey /etc/ssh/ssh_host_ed25519_key
+          Port 8022
+          AllowUsers nix-on-droid
+          PasswordAuthentication No
+          KbdInteractiveAuthentication No
+          Subsystem sftp ${cfg.nixOnDroid.opensshPkg}/libexec/sftp-server
+        ''} -D";
+        switch-nixondroid = "nix-on-droid switch -F path:/data/data/com.termux.nix/files/home/.config/nix-on-droid#nix-on-droid";
+      };
       syntaxHighlighting.enable = true;
     };
   };
