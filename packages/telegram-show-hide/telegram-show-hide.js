043ff7ce72839cf48a3b75b7d17f86126f06487c
David Wronek <david.wronek@mainlining.org>
2026-09-10 18:05:49 +0200

    telegram-show-hide: fix telegram resetting to initial window
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- packages/telegram-show-hide/telegram-show-hide.js
+++ packages/telegram-show-hide/telegram-show-hide.js
@@ -1,18 +1,43 @@
 registerShortcut("Show/Hide Telegram", "Toggle visibility of the Telegram window", "Meta+F1", function () {
+  callDBus(
+    "org.kde.StatusNotifierWatcher",
+    "/StatusNotifierWatcher",
+    "org.freedesktop.DBus.Properties",
+    "Get",
+    "org.kde.StatusNotifierWatcher",
+    "RegisteredStatusNotifierItems",
+    function (systray) {
+      for (var i = 0; i < systray.length; i++) {
+        const service = systray[i].split("/")[0];
+        callDBus(
+          service,
+          "/StatusNotifierItem",
+          "org.freedesktop.DBus.Properties",
+          "Get",
+          "org.kde.StatusNotifierItem",
+          "Id",
+          function (id) {
+            if (id == "TelegramDesktop") {
+              callDBus(
+                service,
+                "/StatusNotifierItem",
+                "org.kde.StatusNotifierItem",
+                "Activate",
+                0,
+                0,
+                function () {},
+              );
+            }
+          },
+        );
+      }
+    },
+  );
+
   const clients = workspace.stackingOrder;
   for (var i = 0; i < clients.length; i++) {
     if (clients[i].resourceClass == "org.telegram.desktop") {
-      clients[i].closeWindow();
-      return;
+      workspace.raiseWindow(clients[i]);
     }
   }
-
-  callDBus(
-    "org.telegram.desktop",
-    "/org/telegram/desktop",
-    "org.freedesktop.Application",
-    "Activate",
-    {},
-    function () {},
-  );
 });
